445a15ef6d91f814f596e11c7a41c48f1b52657a
mattismeyer <mattismeyer@gmx.de>
2020-01-28 15:52:06 +0100

    Managing Data finished

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,8 +13,15 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
-  constructor(private route: ActivatedRoute,) 
-  { }
+  addToCart(product){
+    window.alert('Your Product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    )  { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
